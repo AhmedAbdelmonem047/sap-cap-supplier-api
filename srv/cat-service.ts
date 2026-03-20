4f7bb1167204f24f6fc8017acd8fa0864f35b1b0
AhmedAbdelmonem047 <ahmedabdelmonem047@gmail.com>
2026-03-20 19:49:30 +0200

    Add external API integration

--- srv/cat-service.ts
+++ srv/cat-service.ts
@@ -1,4 +1,5 @@
 import cds, { Request } from "@sap/cds";
+import axios from "axios";
 
 
 export default class CatlogService extends cds.ApplicationService {
@@ -25,6 +26,23 @@ export default class CatlogService extends cds.ApplicationService {
             const { rating } = req.data;
             if (rating !== undefined && (rating < 1 || rating > 5)) {
                 req.error(400, 'Product review rating must be between 1 and 5');
+            }
+        });
+
+        // ******* External API Integration ******* //
+        this.before('CREATE', 'Products', async (req: Request) => {
+            const { category } = req.data;
+            if (category) {
+                try {
+                    const response = await axios.get('https://fakestoreapi.com/products');
+                    const products = response.data;
+                    const matchedProduct = products.find((p: any) => p.category === category);
+                    if (matchedProduct && matchedProduct.rating && matchedProduct.rating.rate)
+                        req.data.externalRating = matchedProduct.rating.rate;
+
+                } catch (error) {
+                    console.warn('Failed to fetch from external API, ignoring error to maintain creation.', error);
+                }
             }
         });
     }
